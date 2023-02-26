7e2f3ef3ce43a77e1f52cbd190882549e4a58806
Charles Vitry <45233151+CharlesVitry@users.noreply.github.com>
2023-02-26 10:33:09 +0000

    update web

--- projet-ima-ihm/src/main/java/fr/ima/ihm/controller/SommePartsController.java
+++ projet-ima-ihm/src/main/java/fr/ima/ihm/controller/SommePartsController.java
@@ -15,7 +15,7 @@ public class SommePartsController {
     @GetMapping("/sum-nombre")
     public String afficherSommeInvestis(Model model) {
         model.addAttribute("somme", dao.sum_nombre());
-        return "sommeInvectis";
+        return "listeAdherents"; // affichage sur la même page que la liste des adhérents.
     }
 
 
